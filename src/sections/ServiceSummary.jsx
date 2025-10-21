aae62e8b84bc59acaaebfdcca573dcd78c3e385a
Lucky Nkosi <lucky.nkosi@ogilvy.co.za>
2025-10-21 03:16:48 +0200

    update service parralex

--- src/sections/ServiceSummary.jsx
+++ src/sections/ServiceSummary.jsx
@@ -36,28 +36,28 @@ const ServiceSummary = () => {
   return (
     <section className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive">
       <div id="title-service-1">
-        <p>Architucture</p>
+        <p>Strategy</p>
       </div>
       <div
         id="title-service-2"
         className="flex items-center justify-center gap-3 translate-x-16"
       >
-        <p className="font-normal">Development</p>
+        <p className="font-normal italic">Branding</p>
         <div className="w-10 h-1 md:w-32 bg-gold" />
-        <p>Deployment</p>
+        <p>Management</p>
       </div>
       <div
         id="title-service-3"
         className="flex items-center justify-center gap-3 -translate-x-48"
       >
-        <p>APIs</p>
+        <p>PR</p>
         <div className="w-10 h-1 md:w-32 bg-gold" />
-        <p className="italic">Frontends</p>
+        <p className="italic">Campaigns</p>
         <div className="w-10 h-1 md:w-32 bg-gold" />
-        <p>Scalability</p>
+        <p>Community</p>
       </div>
       <div id="title-service-4" className="translate-x-48">
-        <p>Databases</p>
+        <p>Analytics</p>
       </div>
     </section>
   );
